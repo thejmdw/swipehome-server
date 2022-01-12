71243996752cd5833034c39a878eac16401245a2
Jonathan Watson <thejmdw@gmail.com>
2022-01-11 19:48:45 -0500

    tweaked app_user and added user_type

--- swipehomeapi/models/app_user.py
+++ swipehomeapi/models/app_user.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .user_type import UserType
 
 
 class AppUser(models.Model):
     
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatarURL = models.CharField(max_length=50)
-    userTypeID = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(2)])
+    userTypeID = models.ForeignKey(UserType, on_delete=models.DO_NOTHING, related_name='types')
     firstTimeUser = models.BooleanField
