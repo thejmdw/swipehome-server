b6dc14b14c1d823c1641d8db640cae72c2d2f52b
Jonathan Watson <thejmdw@gmail.com>
2022-01-12 21:10:25 -0500

    added Search model

--- swipehomeapi/models/search.py
+++ swipehomeapi/models/search.py
@@ -7,3 +7,4 @@ class Search(models.Model):
     postal_code = models.CharField(max_length=5)
     userId = models.ForeignKey("User", on_delete=models.CASCADE)
     userTypeId = models.ForeignKey("UserType", on_delete=models.CASCADE)
+    
