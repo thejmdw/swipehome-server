9e624ddd4a2cd6f35de8092a7a55d1a86588e1a7
Jonathan Watson <thejmdw@gmail.com>
2022-01-17 19:39:33 -0500

    added message model

---
+++ swipehomeapi/models/message.py
@@ -0,0 +1,11 @@
+from django.db import models
+from .app_user import AppUser
+
+class Message(models.Model):
+    
+     userId = models.ForeignKey(AppUser, on_delete=models.DO_NOTHING)
+     recipientId = models.IntegerField()
+     text = models.CharField(max_length=500)
+     timestamp = models.IntegerField()
+     unread = models.BooleanField(default="true")
+     
