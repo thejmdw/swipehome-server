94ba7102dbc6240ac66e3ffda9cdbb8dcab899e5
Jonathan Watson <thejmdw@gmail.com>
2022-01-17 21:52:19 -0500

    added Address model

---
+++ swipehomeapi/models/address.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Address(models.Model):
+    
+    line = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state_code = models.CharField(max_length=2)
+    postal_code = models.CharField(max_length=5)
