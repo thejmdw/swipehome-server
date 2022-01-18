879df96ff14d88c69ee7a064cc82b217e2352970
Jonathan Watson <thejmdw@gmail.com>
2022-01-17 22:04:43 -0500

    added address fixture

--- swipehomeapi/models/__init__.py
+++ swipehomeapi/models/__init__.py
@@ -1,3 +1,7 @@
 from .app_user import AppUser
 from .user_type import UserType
 from .search import Search
+from .fave import Fave
+from .message import Message
+from .house import House
+from .address import Address
--- swipehomeapi/models/search.py
+++ swipehomeapi/models/search.py
@@ -1,10 +1,12 @@
 from django.db import models
+from .app_user import AppUser
+from .user_type import UserType
 
 class Search(models.Model):
     
     city = models.CharField(max_length=50)
     state_code = models.CharField(max_length=2)
     postal_code = models.CharField(max_length=5)
-    userId = models.ForeignKey("User", on_delete=models.CASCADE)
-    userTypeId = models.ForeignKey("UserType", on_delete=models.CASCADE)
+    userId = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    userTypeId = models.ForeignKey(UserType, on_delete=models.CASCADE)
     
